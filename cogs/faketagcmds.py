c58d19f8f54be06c574d68671d23485bf1f7e12a
JaWarrior12 <jacob.meier@cox.net>
2024-06-07 17:52:50 -0400

    Fix submitFakeTagger Bug

--- cogs/faketagcmds.py
+++ cogs/faketagcmds.py
@@ -55,7 +55,7 @@ class FakeTagCmds(commands.Cog, name="Fake Tag Database Commands",description="A
             data=lists.readFakeTags()
             def find_route(data2, route_no):
                 return list(filter(lambda y: y.get("hexcode") == str(route_no), data2))
-            matchingHexs=find_route(data,hexcode)
+            matchingHexs=find_route(data["fakeTaggedShips"],hexcode)
             if len(matchingHexs)>=1:
                 submId=matchingHexs[0]["submissionId"]
                 #submId=data["fakeTaggedShips"][matchingHexs[0]]["submissionId"]

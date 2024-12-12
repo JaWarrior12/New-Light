532e927cbcefd72d29f7821fbed0a6171778eab1
JaWarrior12 <jacob.meier@cox.net>
2024-12-11 22:51:56 -0500

    New script given to n!mlc

--- cogs/setupcmds.py
+++ cogs/setupcmds.py
@@ -317,6 +317,8 @@ class SetupCmds(commands.Cog, name="Server Commands",description="Server Setup C
       if lists.readdataE()[str(ctx.message.guild.id)]["memrole"]==0:
         await ctx.send("Member Role Not Configured, Please use /serverconfig to designate your member role.")
       else:
+        cuts=[]
+        ctnt3=""
         gid=str(ctx.message.guild.id)
         #lists.logback(ctx,msg=None)
         memchan=lists.readdataE()[str(ctx.message.guild.id)]["memchan"]
@@ -324,11 +326,8 @@ class SetupCmds(commands.Cog, name="Server Commands",description="Server Setup C
         nmesg=await channel.send("Member List Message")
         groles=ctx.message.guild.roles
         #message=await channel.fetch_message(nmesg)
-        #print(nmesg.id)
+        #data[str(guild.id)]["memmsg"]=int(nmesg.id)
         data=lists.readdataE()
-        data[str(ctx.message.guild.id)]["memmsg"]=int(nmesg.id)
-        lists.setdataE(data)
-        await ctx.send("Message Configured, Adding Members")
         memrole=ctx.message.guild.get_role(int(data[gid]["memrole"]))
         ranks=[]
         rnkord=[]
@@ -361,6 +360,11 @@ class SetupCmds(commands.Cog, name="Server Commands",description="Server Setup C
                   else:
                     global roleb
                     roleb=ctx.message.guild.get_role(rid2)
+                    if groles.index(roleb) in rnkord:
+                      pass
+                    else:
+                      #ranks.append(rnk)
+                      rnkord.append(groles.index(roleb))
                 getNextRole(roleIndex)
               else:
                 if groles.index(roleb) in rnkord:
@@ -370,7 +374,7 @@ class SetupCmds(commands.Cog, name="Server Commands",description="Server Setup C
                   rnkord.append(groles.index(roleb))
         gidranks=ctx.message.guild.roles
         rnkord.sort(reverse=True)
-        #print(rnkord)
+        #print(f'rnkord == {rnkord}')
         checkedMems=[]
         for x in rnkord:
           rle=gidranks[x].id
@@ -393,49 +397,78 @@ class SetupCmds(commands.Cog, name="Server Commands",description="Server Setup C
                 else:
                   pass
             rmlist.append(rmlistb)
-        #await ctx.send(rmlist)
-        #await ctx.send(ranks)
-        ctnt=""
-        #print(f"ranks == {ranks}")
-        for x in ranks:
-          rmlistIndex=ranks.index(x)
-          def mlcOld():
-            rle=ctx.message.guild.get_role(x)
-            if rle.id==memrole.id:
-              pass
-            elif any([x.mention in ctnt for x in rle.members]):
-              pass
-            elif len(rle.members)>=1:
-              ctnt=ctnt+"\n\n∙"+rle.mention+" ("+str(len(rle.members))+") :"
-            else:
-              pass
-            for us in mems:
-              if rle in us.roles and ctnt.find(us.mention)==-1 and memrole in us.roles:
-                ctnt=ctnt+"\n--"+us.mention
-                mems.remove(us)
+          #print(f'rmlist == {rmlist}')
+          #await ctx.send(rmlist)
+          #await ctx.send(ranks)
+          ctnt=""
+          #print(f"ranks == {ranks}")
+          for x in ranks:
+            rmlistIndex=ranks.index(x)
+            def tempStop():
+              rle=ctx.message.guild.get_role(x)
+              if rle.id==memrole.id:
+                pass
+              elif any([x.mention in ctnt for x in rle.members]):
+                pass
+              elif len(rle.members)>=1:
+                ctnt=ctnt+"\n\n∙"+rle.mention+" ("+str(len(rle.members))+") :"
               else:
                 pass
-          async def mlcNew(x):
-            ctnt2=""
-            rle=ctx.message.guild.get_role(x)
-            if rle.id==memrole.id:
-              pass
-            elif any([x.mention in ctnt2 for x in rle.members]):
-              pass
-            elif len(rle.members)>=1:
-              ctnt2=ctnt2+"\n\n∙"+rle.mention+" ("+str(len(rle.members))+") :"
-            else:
-              pass
-              for mem in rle.members:
-                us=await ctx.message.guild.fetch_member(mem.id)
-                if rle in us.roles and ctnt2.find(us.mention)==-1 and memrole in us.roles:
-                  ctnt2=ctnt2+"\n--"+us.mention
+              for us in mems:
+                if rle in us.roles and ctnt.find(us.mention)==-1 and memrole in us.roles:
+                  ctnt=ctnt+"\n--"+us.mention
+                  mems.remove(us)
                 else:
                   pass
-            #print(ctnt2)
-            return ctnt2
-          ctnt = ctnt + await mlcNew(x)
-        await nmesg.edit(content=ctnt)
+            async def tempNew(x):
+              ctnt2=""
+              rle=ctx.message.guild.get_role(x)
+              if rle.id==memrole.id:
+                pass
+              elif any([x.mention in ctnt2 for x in rle.members]):
+                pass
+              elif len(rle.members)>=1:
+                ctnt2=ctnt2+"\n\n∙"+rle.mention+" ("+str(len(rle.members))+") :"
+              else:
+                pass
+              for mem in rle.members:
+                  us=await ctx.message.guild.fetch_member(mem.id)
+                  if rle in us.roles and ctnt2.find(us.mention)==-1 and memrole in us.roles:
+                    ctnt2=ctnt2+"\n--"+us.mention
+                    #rle.members.remove(mem)
+                  else:
+                    pass
+              #print(ctnt2)
+              return ctnt2
+            nextBit=await tempNew(x)
+            if int(len(ctnt) + len(nextBit)) < 1930:
+              ctnt = ctnt + nextBit
+            else:
+              cuts.append(ctnt)
+              ctnt=nextBit
+          nextItem=ctnt
+          #print(len(ctnt))
+          ctnt3Len=len(ctnt3)
+          nextItemLen=len(nextItem)
+          #print(f"ctnt3Len={ctnt3Len}; nextItemLen={nextItemLen}")
+          if int(ctnt3Len+nextItemLen) < 1930:
+            ctnt3+=nextItem
+            #print("continue")
+          else:
+            cuts.append(ctnt3)
+            ctnt3=""
+            #print("cut")
+            #await nmesg.edit(content=ctnt)
+        cuts.append(ctnt3)
+        try:
+          await channel.purge()
+        except:
+          pass
+        #print(cuts)
+        for cut in cuts:
+          if len(cut)>0:
+            mesg=await channel.send("temp")
+            await mesg.edit(content=cut)
         await ctx.send(f"Member List Created: {nmesg.jump_url}")
     else:
       await ctx.send("Not authorized to use leadership commands in this server")

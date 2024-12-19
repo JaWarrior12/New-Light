a2f613d667924b9a74e8ac9ae921532f803772a9
JaWarrior12 <97358009+JaWarrior12@users.noreply.github.com>
2024-12-19 14:16:04 -0500

    Add perm check command (Needs Testing)

--- cogs/adcmds.py
+++ cogs/adcmds.py
@@ -427,6 +427,26 @@ class AdCmds(commands.Cog, name="Dev Admin Tools", description="New Light Develo
       await guild.Owner.send(f'{ctx.message.author.name} has remotely changed {(option)} in `{guild.name} ({guild.id})` from {prevVal} to {val}')
     else:
       await ctx.send("Sorry, only developers can use this command.")
-    
+
+  @commands.command(name="remoteCheckPerms",aliases=["rcp","remotePerms"],help="Allows the developer to remotely check the perms New Light has in a server. Primarally used for troubleshooting without needing to give the dev perms.")
+  async def remoteCheckPerms(self,ctx,guildID):
+    if ctx.message.author.id in developers:
+      guild=self.bot.get_guild(int(guildID))
+      outputFileName=f"{guild.name}_perms_list.md"
+      outputFile=None
+      try:
+        with open(outputFileName, "a+", encoding="utf-8") as outputFile:
+          outputFile.write(f"# Server: {guild.name} ({guild.id})")
+          for channel in guild.channels:
+            outputFile.write(f"- Channel: {channel.name} ({channel.id})")
+            permsList=[perm for perm in channel.permissions_for(self.bot)]
+            for perm in permsList:
+              outputFile.write(f" - Permission: `{perm[0]}`; Value: `{perm[1]}`")
+      except:
+        os.remove(outputFileName)
+
+    else:
+      await ctx.send("Sorry, only developers can use this command.")
+
 async def setup(bot: commands.Bot):
     await bot.add_cog(AdCmds(bot))
